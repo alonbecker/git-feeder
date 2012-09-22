2d32f0b7db83eb5fc0d82fc1c0182463a48798e9
Alon Becker <alon.becker@hp.com>
2012-09-22 17:04:40 +0300

    test2

---
+++ lib/git_feed/pub.rb
@@ -0,0 +1,18 @@
+require 'rubygems'
+require 'redis'
+require 'json'
+require 'yaml'
+
+CONFIG = YAML.load_file("config.yml") unless defined? CONFIG
+
+
+puts "hello #{CONFIG['host']}"
+
+$redis = Redis.new(:host => "#{CONFIG['host']}", :port => "#{CONFIG['port']}")
+
+data = {"user" => ARGV[1]}
+
+loop do
+    msg = STDIN.gets
+    $redis.publish ARGV[0], data.merge('msg' => msg.strip).to_json
+end
