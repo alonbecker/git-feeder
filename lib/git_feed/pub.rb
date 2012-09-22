567aac4c79d30b84001b211eb45a603f7b31e74a
Alon Becker <alon.becker@hp.com>
2012-09-22 20:46:59 +0300

    got files working as well

--- lib/git_feed/pub.rb
+++ lib/git_feed/pub.rb
@@ -7,10 +7,32 @@ require 'git'
 CONFIG = YAML.load_file("config.yml") unless defined? CONFIG
 git = Git.open ('../..')
 
-$redis = Redis.new(:host => "#{CONFIG['host']}", :port => "#{CONFIG['port']}")
+commits = git.log(2)
+commit = ''
+last_commit = ''
+
+commits.each_with_index do |sha1, idx|
+  commit = git.gcommit(sha1) if idx == 0
+  last_commit = git.gcommit(sha1) if idx == 1
+end
+
+files = Array.new
+
+git.diff(commit, last_commit).each do |file|
+   files.push(file.path)
+end
+
+puts git.gcommit(git.log(1)).name
+ 
 data = 
   {
-    "user" => git.gcommit(git.log(1)).author.name, 
-    "msg" => git.gcommit(git.log(1)).message
+    "user" 	=> commit.author.name, 
+    "email" 	=> commit.author.email,
+    "date" 	=> commit.date.strftime("%Y-%m-%d %H:%M:%S %z"),
+    "msg" 	=> commit.message,
+    "branch" 	=> commit.name,
+    "files" 	=> files
   }
+
+$redis = Redis.new(:host => "#{CONFIG['host']}", :port => "#{CONFIG['port']}")
 $redis.publish 'git', data.to_json
