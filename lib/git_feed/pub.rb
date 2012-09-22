3d748afe2c2c6edc05f83e11eb512600aab3fa7d
Alon Becker <alon.becker@hp.com>
2012-09-22 19:42:02 +0300

    successfully published

--- lib/git_feed/pub.rb
+++ lib/git_feed/pub.rb
@@ -2,17 +2,15 @@ require 'rubygems'
 require 'redis'
 require 'json'
 require 'yaml'
+require 'git'
 
 CONFIG = YAML.load_file("config.yml") unless defined? CONFIG
+git = Git.open ('../..')
 
-
-puts "hello #{CONFIG['host']}"
-
-$redis = Redis.new(:host => "#{CONFIG['host']}", :port => "#{CONFIG['port']}")
-#test3
-data = {"user" => ARGV[1]}
-
-loop do
-    msg = STDIN.gets
-    $redis.publish ARGV[0], data.merge('msg' => msg.strip).to_json
+git.log.each_with_index do |commit, idx|
+  if idx == git.log.size - 1 then
+    $redis = Redis.new(:host => "#{CONFIG['host']}", :port => "#{CONFIG['port']}")
+    data = {"user" => git.gcommit(commit).author.name, "msg" => git.gcommit(commit).message}
+    $redis.publish 'git', data.to_json
+  end 
 end
