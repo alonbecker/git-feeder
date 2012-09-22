b5ca6bcf8295712a197116ad731dbeb45cb76437
Alon Becker <alon.becker@hp.com>
2012-09-22 19:56:26 +0300

    fixed again

--- lib/git_feed/pub.rb
+++ lib/git_feed/pub.rb
@@ -7,11 +7,10 @@ require 'git'
 CONFIG = YAML.load_file("config.yml") unless defined? CONFIG
 git = Git.open ('../..')
 
-git.log.each_with_index do |commit, idx|
-  if idx == 0 then
-    $redis = Redis.new(:host => "#{CONFIG['host']}", :port => "#{CONFIG['port']}")
-    data = {"user" => git.gcommit(commit).author.name, "msg" => git.gcommit(commit).message}
-    $redis.publish 'git', data.to_json
-    break
-  end 
-end
+$redis = Redis.new(:host => "#{CONFIG['host']}", :port => "#{CONFIG['port']}")
+data = 
+  {
+    "user" => git.gcommit(git.log(1)).author.name, 
+    "msg" => git.gcommit(git.log(1)).message
+  }
+$redis.publish 'git', data.to_json
