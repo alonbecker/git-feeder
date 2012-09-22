dc4816eb05b8c5db3e98fcd9dfb4c8e21243f845
Alon Becker <alon.becker@hp.com>
2012-09-22 19:44:36 +0300

    the right build number

--- lib/git_feed/pub.rb
+++ lib/git_feed/pub.rb
@@ -8,9 +8,10 @@ CONFIG = YAML.load_file("config.yml") unless defined? CONFIG
 git = Git.open ('../..')
 
 git.log.each_with_index do |commit, idx|
-  if idx == git.log.size - 1 then
+  if idx == 0 then
     $redis = Redis.new(:host => "#{CONFIG['host']}", :port => "#{CONFIG['port']}")
     data = {"user" => git.gcommit(commit).author.name, "msg" => git.gcommit(commit).message}
     $redis.publish 'git', data.to_json
+    break
   end 
 end
